10e77fae7f437a52e95324cef101613b419d2f46
HanyueW-9 <55716153+HanyueW-9@users.noreply.github.com>
2020-02-11 18:04:24 -0700

    Update stopCar.sh

--- CodeBase/stopCar.sh
+++ CodeBase/stopCar.sh
@@ -1,2 +1,9 @@
-#bin/sh
-echo "stop the car" 
+#!/bin/bash  
+PROC_NAME=$1  
+ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`  
+if [ $ProcNumber -le 0 ];then  
+   result=0  
+else  
+   result=1   
+fi  
+echo ${result}
