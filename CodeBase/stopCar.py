015e8e3f9aa029c67bacc3a3e50e9beea5dcb865
HanyueW-9 <55716153+HanyueW-9@users.noreply.github.com>
2020-02-21 14:30:38 -0700

    Update stopCar.py

--- CodeBase/stopCar.py
+++ CodeBase/stopCar.py
@@ -4,10 +4,11 @@ import os, sys, string
 import re
 
 result = 1
+
 if __name__ == '__main__':
     p = subprocess.getoutput("pgrap roscore")
-    subprocess.getoutput("kill %s" %p)
-    result = 0
+    if subprocess.getoutput("kill %s" %p) == True:
+        result = 0
 
 if(result == 0):
     print("The roscore been closed, the car stop successfully")
