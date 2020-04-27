b57c5a5cc6d745a4d3fa5c88231efb71de86581b
Jordan Wright <jaw566@nau.edu>
2020-04-27 13:32:36 -0700

    changed password setting to be a command line argument given to the call of setup_app.sh

--- CodeBase/App/setup_app.sh
+++ CodeBase/App/setup_app.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Description: this script is used to set up the RosConnect system by installing all necessary dependencies,
+#              changing the password for running the vehicles via 'ssh', and creating a desktop icon for starting
+#              the RosConnect application
+
 cd ~/Capstone/CodeBase/App
-./mk_all_password_instances.py password_here
+
+./mk_all_password_instances.py $1 # password set via first command line argument
+
 /bin/cp RosConnect.desktop $HOME/Desktop
+
 ./init_desktop_file.py
+
 /bin/cp run_gui.sh $HOME 
+
 /bin/cp logo.png $HOME
 
 
