7e014f3f50795fc1c767b1b7c807dc28b29702e3
Jordan Wright <jaw566@nau.edu>
2020-05-01 09:11:50 -0700

    catch incorrect number of args to setup script

--- App/setup.sh
+++ App/setup.sh
@@ -4,11 +4,17 @@
 #              changing the password for running the vehicles via 'ssh', and creating a desktop icon for starting
 #              the RosConnect application
 
+if [ "$#" -ne 4 ]; then
+    echo "You must run setup with exactly 4 command line arguments as follows:"
+    echo "./setup.sh [host_password] [vehicle_ip] [vehicle_hostname] [vehicle_password]"
+    exit
+fi
+
 cd ~/RosConnect/App
 
 ./install.sh $1 # raspberry Pi password
 
-./mk_hostname_and_password.py $2 $3 $4 # vehicle host_id hostname and password (in that order)
+./mk_hostname_and_password.py $2 $3 $4 # vehicle ip hostname and password (in that order)
 
 /bin/cp RosConnect.desktop $HOME/Desktop
 
